725cdbf3590469c401558b03d1f0184a4e21b495
Eduardo <eeadomingues@gmail.com>
2023-11-03 18:10:04 -0300

    dados livros

--- livros-react/src/App.tsx
+++ livros-react/src/App.tsx
@@ -1,9 +1,34 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LivroLista from "./LivroLista";
+import LivroDados from "./LivroDados";
 
 function App() {
-  return <LivroLista />;
+  return (
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Catálogo
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/dados" className="nav-link">
+                Novo
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<LivroLista />} />
+        <Route path="/dados" element={<LivroDados />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
